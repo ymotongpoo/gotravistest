88a9920e0bf03f394588def0cd5f3b52ff8f9840
ymotongpoo <ymotongpoo AT gmail.com>
2012-10-21 17:02:38 +0900

    changed to fail the test

--- sample_test.go
+++ sample_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestAdd(t *testing.T) {
 	const n, m = 2, 3
-	const out = 5
+	const out = 6
 	if x := Add(n, m); x != out {
 		t.Errorf("Add(%v, %v) = %v, want %v", n, m, x, out)
 	}
